e64f9dabbc36c1c263b850c9b3fda484e4614bc7
eyenseo <eyenseo@gmail.com>
2013-05-12 16:10:00 +0200

    Add logTemperature function to Ingredient Part of Exercise 2a ...

--- src/js/Ingredient.js
+++ src/js/Ingredient.js
@@ -12,25 +12,39 @@ function Ingredient(context, sx, sy, w, h, imgPath, zOrder, draggable, name, hea
 	this.COOLING = 2;
 	this.COOL = 3;
 	this.temperatureState = this.COOL;
+
+	this.logCounter = 0;
 }
 
 Ingredient.prototype = new VisualRenderObject();
 Ingredient.prototype.constructor = Ingredient;
 
 Ingredient.prototype.updateTemperature = function(temperature) {
-	if(this.temperature < temperature) {
-		this.temperature = this.temperature + temperature * this.HEATRISINGRATE;
-		this.temperatureState = this.HEATING;
-		if(this.temperature > temperature) {
-			this.temperature = temperature;
-			this.temperatureState = this.HEATED;
-		}
-	} else if(this.temperature > temperature) {
-		this.temperature = this.temperature - temperature * this.HEATRISINGRATE;
-		this.temperatureState = this.COOLING;
+	if(this.temperature != temperature) {
 		if(this.temperature < temperature) {
-			this.temperature = temperature;
-			this.temperatureState = this.COOL;
+			this.temperature = this.temperature + temperature * this.HEATRISINGRATE;
+			this.temperatureState = this.HEATING;
+			if(this.temperature > temperature) {
+				this.temperature = temperature;
+				this.temperatureState = this.HEATED;
+			}
+		} else if(this.temperature > temperature) {
+			this.temperature = this.temperature - temperature * this.HEATRISINGRATE;
+			this.temperatureState = this.COOLING;
+			if(this.temperature < temperature) {
+				this.temperature = temperature;
+				this.temperatureState = this.COOL;
+			}
 		}
+	}
+	this.logTemperature();
+};
+
+Ingredient.prototype.logTemperature = function() {
+	if(this.logCounter == 120) {
+		this.logCounter = 0;
+		console.log(this.temperature);
+	} else {
+		this.logCounter++;
 	}
 };
