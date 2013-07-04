3b97340ac9d86d22323baf072120b620c0bc9fd3
Eyenseo <eyenseo@gmail.com>
2013-07-04 15:29:52 +0200

    Remove old Kitchen

--- src/js/Bowl.js
+++
@@ -1,8 +0,0 @@
-//TODO Doc
-function Bowl(context, data) {
-	ContainerStuff.call(this, context, data);
-	this.setHitZone(11, 9, 95, 22);
-}
-
-Bowl.prototype = Object.create(ContainerStuff.prototype);
-Bowl.prototype.constructor = Bowl;
--- src/js/Sieve.js
+++
@@ -1,7 +0,0 @@
-function Sieve(context, data) {
-	ContainerStuff.call(this, context, data);
-	//	this.setHitZone(0, 0, 0, 0);
-}
-
-Sieve.prototype = Object.create(ContainerStuff.prototype);
-Sieve.prototype.constructor = Sieve;
